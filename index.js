3839da0f21491142a3238ae256378bd0aa400d4a
Frederick Pokoj <nachhalttiger@frederick-pokoj.de>
2022-04-06 11:22:48 +0200

    First

---
+++ index.js
@@ -0,0 +1,22 @@
+const PORT = 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    console.log(req.body)
+    res.send()
+})
+
+app.get('/response', (req, res) => {
+
+})
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
